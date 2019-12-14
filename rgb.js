ff63e9a9f9bd1e95ac111a58fc8ec15ba9b311ae
Meifang Lin <meiflin123@gmail.com>
2019-12-13 17:24:16 -0800

    create random color

--- rgb.js
+++ rgb.js
@@ -1,20 +1,13 @@
-
-//hardcode color array
-//hardcode solution 
-
-
-const solution = 'red'
+const solutionSpan = document.querySelector('.title span')
 const squares = document.querySelectorAll('.square');
 const title = document.querySelector('.title')
 const span = document.querySelector('.bar span')
 
-
-//once page load, call init.
 const init = () => {
   setupSquares();
 }
 
-
+//once page load, call init.
 // pass 6 colors to each squares
 //add click event listener to all squares
   // compare guessed color and solution: 
@@ -26,8 +19,13 @@ const init = () => {
       //guessed square's background = body background
 
 const setupSquares = () => {
+  const colors = generateColor(6)
+  const solution = colors[Math.floor(Math.random() * 6)]
+  solutionSpan.textContent = solution;
+
+  console.log('square', colors)
   for(let i = 0; i < squares.length; i++) {
-    const pickedColor = squares[i].style.backgroundColor = color[i];
+    const pickedColor = squares[i].style.backgroundColor = colors[i];
 
     squares[i].addEventListener('click', ()=>{
       if(pickedColor === solution) { 
@@ -47,6 +45,22 @@ const setupSquares = () => {
 }
 
 
+// for loop num, call random()
 
+const generateColor = num => {
+  let colors = [];
+  for (let i = 0; i < num; i++) {
+    colors.push(random());
+
+  }
+  return colors;
+}
+
+const random = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return 'rgb(' + r +', ' + g + ', ' + b + ')';
+}
 
 init();
