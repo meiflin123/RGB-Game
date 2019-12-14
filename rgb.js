2e366f137824081681d6f5ea0174032aa4942c2b
Meifang Lin <meiflin123@gmail.com>
2019-12-13 16:46:46 -0800

    setup event listener for each square

--- rgb.js
+++ rgb.js
@@ -2,8 +2,11 @@
 //hardcode color array
 //hardcode solution 
 
-const color = ['red', 'blue', 'green', 'gray', 'yellow', 'white']
+
 const solution = 'red'
+const squares = document.querySelectorAll('.square');
+const title = document.querySelector('.title')
+const span = document.querySelector('.bar span')
 
 
 //once page load, call init.
@@ -11,27 +14,39 @@ const init = () => {
   setupSquares();
 }
 
+
 // pass 6 colors to each squares
 //add click event listener to all squares
   // compare guessed color and solution: 
-    //if same, alert correct
-    // if diff, guessed square's background = body background 
+    //if same, 
+      //make title the solution color.
+      //make rest sqaures solution color.
+      // display'correct' to header bar.
+    // if diff, 
+      //guessed square's background = body background
 
 const setupSquares = () => {
-
-  const squares = document.querySelectorAll('.square');
   for(let i = 0; i < squares.length; i++) {
-
     const pickedColor = squares[i].style.backgroundColor = color[i];
+
     squares[i].addEventListener('click', ()=>{
       if(pickedColor === solution) { 
-        alert('right')
+        title.style.backgroundColor = solution
+        console.log(span)
+        span.textContent= 'correct'
+        for (let i = 0; i < squares.length; i++) {
+          squares[i].style.backgroundColor = solution;
+
+        }
       } else {
         squares[i].style.backgroundColor = "#232323"
+        span.textContent ="Try Again"
       } 
-
     })
   }    
 }
+
+
+
 
 init();
